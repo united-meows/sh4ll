8a28e1473ed272a5791f7a72334539a1eee92910
deadghost <69243988+deadghost2173@users.noreply.github.com>
2021-01-06 18:28:15 +0300

    replaced more shit
    zaaaaaaaaaaaaaaaaa

--- util/MinecraftFontRenderer.java
+++ util/MinecraftFontRenderer.java
@@ -60,50 +60,6 @@ public class MinecraftFontRenderer extends CFont
         return this.drawString(text, (float) (x - this.getStringWidth(text) / 2), y, color);
     }
 
-    public Color getChromaColor(final double x, final double y, final double offsetScale) {
-        final float v = 2000.0f;
-        return new Color(Color.HSBtoRGB((float)((System.currentTimeMillis() - x * 10.0 * offsetScale - y * 10.0 * offsetScale) % v) / v, 0.8f, 0.8f));
-    }
-
-    public void drawChromaStringCustomChroma(final String text, int x, final int y, final double offsetScale) {
-        char[] charArray;
-        for (int length = (charArray = text.toCharArray()).length, j = 0; j < length; ++j) {
-            final char c = charArray[j];
-            final int i = this.getChromaColor(x, y, offsetScale).getRGB();
-            final String tmp = String.valueOf(c);
-            this.drawString(tmp, (float)x, (float)y, i);
-            x += this.getStringWidth(tmp);
-        }
-    }
-
-    public Color getChromaColorCustom(final double x, final double y, final double offsetScale, final float divider, final float saturation, final float brightness) {
-        final float v = divider;
-        return new Color(Color.HSBtoRGB((float)((System.currentTimeMillis() - x * 10.0 * offsetScale - y * 10.0 * offsetScale) % v) / v, saturation, brightness));
-    }
-
-    public void drawChromaStringFullyCustom(final String text, float x, final float y, final double offsetScale, final float divider, final float saturation, final float brightness) {
-        char[] charArray;
-        for (int length = (charArray = text.toCharArray()).length, j = 0; j < length; ++j) {
-            final char c = charArray[j];
-            final int i = this.getChromaColorCustom(x, y, offsetScale, divider, saturation, brightness).getRGB();
-            final String tmp = String.valueOf(c);
-            this.drawString(tmp, x, y, i);
-            x += this.getStringWidth(tmp);
-        }
-    }
-
-    public void drawChromaString(final String text, int x, final int y, final double offsetScale) {
-        char[] charArray;
-        for (int length = (charArray = text.toCharArray()).length, j = 0; j < length; ++j) {
-            final char c = charArray[j];
-            final int i = this.getChromaColor(x, y, offsetScale).getRGB();
-            final String tmp = String.valueOf(c);
-            this.drawString(tmp, (float)x, (float)y, i);
-            x += this.getStringWidth(tmp);
-        }
-    }
-
-
     public void drawBorderedString(final String text, final float x, final float y, final int color) {
         this.drawString(text, x - 0.5f, y, 0);
         this.drawString(text, x + 0.5f, y, 0);
@@ -117,7 +73,9 @@ public class MinecraftFontRenderer extends CFont
         if (actualString == null) {
             return 0.0f;
         }
-        final String renamedString = actualString.replace('\u0131', 'i').replace('\u011f', 'g').replace('\u00fc', 'u').replace('\u015f', 's').replace('\u00f6', 'o').replace('\u00e7', 'c').replace('\u0130', 'I');
+        final String renamedString = actualString.replace('ı', 'i').replace('İ', 'I').replace('ğ', 'g').replace('Ğ', 'G').replace('ç', 'c')
+				.replace('Ç', 'C').replace('ü', 'u').replace('Ü', 'U').replace('ş', 's').replace('Ş', 'S')
+				.replace('ö', 'o').replace('Ö', 'O');
         if (color == 553648127) {
             color = 16777215;
         }
@@ -263,7 +221,9 @@ public class MinecraftFontRenderer extends CFont
         if (text == null) {
             return 0;
         }
-        text = text.replace('\u0131', 'i').replace('\u011f', 'g').replace('\u00fc', 'u').replace('\u015f', 's').replace('\u00f6', 'o').replace('\u00e7', 'c').replace('\u0130', 'I');
+        text = text.replace('ı', 'i').replace('İ', 'I').replace('ğ', 'g').replace('Ğ', 'G').replace('ç', 'c')
+				.replace('Ç', 'C').replace('ü', 'u').replace('Ü', 'U').replace('ş', 's').replace('Ş', 'S')
+				.replace('ö', 'o').replace('Ö', 'O');
         double width = 0;
         CharData[] currentData = this.charData;
         boolean bold = false;
@@ -332,20 +292,7 @@ public class MinecraftFontRenderer extends CFont
         this.texItalicBold = this.setupTexture(this.font.deriveFont(3), this.antiAlias, this.fractionalMetrics, this.boldItalicChars);
     }
 
-    private String toRandom(final String text) {
-        String newText = "";
-        final String allowedCharactersOG = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8£\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1ªº¿®¬½¼¡«»\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261±\u2265\u2264\u2320\u2321\u00f7\u2248°\u2219·\u221a\u207f²\u25a0\u0000";
-        final String allowedCharacters = "#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-        char[] var8;
-        for (int var7 = (var8 = text.toCharArray()).length, var9 = 0; var9 < var7; ++var9) {
-            final char c = var8[var9];
-            if (ChatAllowedCharacters.isAllowedCharacter(c)) {
-                final int index = this.fontRandom.nextInt("#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~".length());
-                newText = String.valueOf(String.valueOf(newText)) + "#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~".toCharArray()[index];
-            }
-        }
-        return newText;
-    }
+    
 
     private void drawLine(final double x, final double y, final double x1, final double y1, final float width) {
         GL11.glDisable(3553);
@@ -359,7 +306,9 @@ public class MinecraftFontRenderer extends CFont
 
     public List<String> wrapWords(String text, final double width) {
         final List finalWords = new ArrayList();
-        text = text.replace('\u0131', 'i').replace('\u011f', 'g').replace('\u00fc', 'u').replace('\u015f', 's').replace('\u00f6', 'o').replace('\u00e7', 'c').replace('\u0130', 'I');
+        text = text.replace('ı', 'i').replace('İ', 'I').replace('ğ', 'g').replace('Ğ', 'G').replace('ç', 'c')
+				.replace('Ç', 'C').replace('ü', 'u').replace('Ü', 'U').replace('ş', 's').replace('Ş', 'S')
+				.replace('ö', 'o').replace('Ö', 'O');
         if (this.getStringWidth(text) > width) {
             final String[] words = text.split(" ");
             String currentWord = "";
@@ -403,7 +352,9 @@ public class MinecraftFontRenderer extends CFont
         final List finalWords = new ArrayList();
         String currentWord = "";
         char lastColorCode = '\uffff';
-        string = string.replace('\u0131', 'i').replace('\u011f', 'g').replace('\u00fc', 'u').replace('\u015f', 's').replace('\u00f6', 'o').replace('\u00e7', 'c').replace('\u0130', 'I');
+        string = string.replace('ı', 'i').replace('İ', 'I').replace('ğ', 'g').replace('Ğ', 'G').replace('ç', 'c')
+				.replace('Ç', 'C').replace('ü', 'u').replace('Ü', 'U').replace('ş', 's').replace('Ş', 'S')
+				.replace('ö', 'o').replace('Ö', 'O');
         final char[] chars = string.toCharArray();
         for (int i = 0; i < chars.length; ++i) {
             final char c = chars[i];
